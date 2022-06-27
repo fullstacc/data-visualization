b9171185b38096f5e59eab14f26ff452470e6bd7
Glitch (dataviz-with-react-and-d3) <none>
2022-06-27 00:31:49 +0000

    refactor; corrected data source, data table

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,27 +1,11 @@
 import * as d3 from "d3";
 import React, { useRef, useEffect, useState } from "react";
-import "./DataTable.css"
+import "./DataTable.css";
 
 // It took way too long to realize I don't need to make this a d3 table
 
-const dataSource =
-  "https://gist.githubusercontent.com/fullstacc/3f0b9cf691d27d7c3c20b582e6a4c84a/raw/0832453379cd6368558d51258786e5241bc2010e/eurovision.csv";
-const cols = [
-  "Date",
-  "Host City",
-  "Language",
-  "maxPoints",
-  "Performer(s)",
-  "Points",
-  "Song",
-  "Songwriter(s)",
-  "Winner",
-  "Year",
-];
-
 // Table Component
-const DataTable = ({data}) => {
-  
+const DataTable = ({ data }) => {
   // const margin = {
   //   top: 30,
   //   right: 20,
@@ -29,47 +13,58 @@ const DataTable = ({data}) => {
   //   left: 20,
   // };
 
-
   // const d3Chart = useRef();
 
-
-
-if (data.length>1){
-  const cols = data.columns
-  return (
-    <div >
-      <table>
-        <tr>
-        {cols.map((col) => {
-            return <th>{col}</th>
-          })}
-        </tr>
-      {data.map((entry) => {
-        return <tr> 
-        <td>{entry["Date"]}</td>
-          <td>{entry["Host city"]}</td>
-          <td>{entry["Language"]}</td>
-          <td>{entry["maxPoints"]}</td>
-          <td>{entry["Performer(s)"]}</td>
-          <td>{entry["Points"]}</td>
-          <td>{entry["Song"]}</td>
-          <td>{entry["Songwriter(s)"]}</td>
-          <td>{entry["Winner"]}</td>
-          <td>{entry["Year"]}</td>
-        </tr>;
-      })}
-        </table>
-    </div>
-  );
-  
-}
-  else {
+  if (data.length > 1) {
+    const cols = data.columns;
+    console.log('these are the columns!', cols)
     return (
-    <div>
-      Data not yet loaded
-      </div>)
+      <div>
+        <table>
+          <tbody>
+          <tr>
+            {cols.map((col) => {
+              return <th>{col}</th>;
+            })}
+          </tr>
+          {data.map((entry) => {
+              console.log('first data entry',entry)
+              
+              
+              // this logic addresses the fact that in 1969, there were (3) winners 
+              let entryYear = entry["Year"]
+              let hostCity = entry["Host city"]
+              let date = entry["Date"]
+              if (entry["Year"].length < 1){
+                entryYear = "1969"
+                hostCity = "Madrid"
+                date = "29 March"
+              }
+             
+              
+              
+            return (<tr>
+              <td>{entryYear}</td>
+              <td>{hostCity}</td>
+              <td>{date}</td>
+              <td>{entry["Winner"]}</td>
+              <td>{entry["Song"]}</td>
+              <td>{entry["Performer(s)"]}</td>
+              <td>{entry["Songwriter(s)"]}</td>
+              <td>{entry["Language"]}</td>
+              <td>{entry["Points"]}</td>
+              <td>{entry['Percentage of\nmaximum possible points']}</td>
+              
+              
+            </tr>)
+          })}
+            </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return <div>Data not yet loaded</div>;
   }
-  
 };
 
 export default DataTable;
--- src/services/dataService.jsx
+++ src/services/dataService.jsx
@@ -1,5 +1,6 @@
-const dataSource =
-  "https://gist.githubusercontent.com/fullstacc/3f0b9cf691d27d7c3c20b582e6a4c84a/raw/0832453379cd6368558d51258786e5241bc2010e/eurovision.csv";
+// const dataSource = "https://gist.githubusercontent.com/fullstacc/3f0b9cf691d27d7c3c20b582e6a4c84a/raw/88aaa3620943b63b70e4c23e4b0831bc5cda9b4d/eurovision.csv"
+const dataSource = "https://gist.githubusercontent.com/fullstacc/3f0b9cf691d27d7c3c20b582e6a4c84a/raw/ff28ca9e84527f1a8798eede096e1c4e31bfcc53/eurovision.csv"
+
 import * as d3 from "d3";
 import axios from "axios";
 
@@ -17,6 +18,7 @@ const DataFetcher = (props) => {
 const getData = () => {
   const csvData = d3.csv(dataSource).then((d) => {
     console.log('d within csvData is', d)
+    console.log('cols within csvData is', d.columns)
     return d;
   })
   // .then((d) => {return d}
